3f5e4fc9699329d2b7e80e61391955a4e33ae6a8
redrebel <cjred77@gmail.com>
2012-03-22 22:48:07 +0900

    maven이 등록된 소스 등록

---
+++ src/main/java/springbook/user/dao/DaoFactory.java
@@ -0,0 +1,41 @@
+package springbook.user.dao;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+
+@Configuration
+public class DaoFactory {
+	@Bean
+	public UserDao userDao() {
+		//return new UserDao(connectionMaker());
+		UserDao userDao = new UserDao();
+		//userDao.setConnectionMaker(connectionMaker());
+		userDao.setDataSource(dataSource());
+		return userDao;
+	}
+	
+	@Bean
+	public ConnectionMaker connectionMaker() {
+		return new DConnectionMaker();
+	}
+	
+	@Bean
+	public DataSource dataSource(){
+		SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+		
+		dataSource.setDriverClass(com.mysql.jdbc.Driver.class);
+		dataSource.setUrl("jdbc:mysql://localhost/springbook");
+		dataSource.setUsername("spring");
+		dataSource.setPassword("book");
+		
+		return dataSource;
+	}
+	/*public UserDao userDao() {
+		ConnectionMaker connectionMaker = new DConnectionMaker();
+		UserDao userDao = new UserDao(connectionMaker);
+		return userDao;
+	}*/
+}
