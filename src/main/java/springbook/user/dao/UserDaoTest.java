771956ba9cb4292613409238d44bcccfbb4478e0
redrebel <cjred77@gmail.com>
2012-03-30 06:28:03 +0900

    count() 추가

--- src/main/java/springbook/user/dao/UserDaoTest.java
+++ src/main/java/springbook/user/dao/UserDaoTest.java
@@ -17,6 +17,28 @@ import springbook.user.dao.*;
 
 public class UserDaoTest {
 	@Test
+	public void count() throws ClassNotFoundException, SQLException {
+		ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+		
+		UserDao dao = context.getBean("userDao", UserDao.class);
+		User user1 = new User("gyumee", "박성철", "springno1");
+		User user2 = new User("leegw700", "이길원", "springno2");
+		User user3 = new User("bumjin", "박범진", "springno3");
+		
+		dao.deleteAll();
+		assertThat(dao.getCount(), is(0));
+		
+		dao.add(user1);
+		assertThat(dao.getCount(), is(1));
+		
+		dao.add(user2);
+		assertThat(dao.getCount(), is(2));
+		
+		dao.add(user3);
+		assertThat(dao.getCount(), is(3));
+	}
+	
+	@Test
 	public void addAndGet() throws ClassNotFoundException, SQLException {
 		
 		//ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
